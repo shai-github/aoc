89aff86f85010742b70b89686cdf09d6163daeaf
shai-github <shaigithub@gmail.com>
2023-12-03 23:49:55 -0600

    add recalibration consideration

--- 2023/day1/trebuchet.py
+++ 2023/day1/trebuchet.py
@@ -1,6 +1,15 @@
 import re
 
 
+DIGIT_STRINGS = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+]
+
+STRING_MAP = {
+    key: str(idx + 1) for idx, key in enumerate(DIGIT_STRINGS)
+}
+
+
 def read_data(pathname: str) -> str:
     """
     Reads data from a file and returns it as a string
@@ -20,19 +29,31 @@ def parse_data(data_string: str) -> list:
     return data_string.split("\n")
 
 
-def find_numbers(data: list):
+def find_numbers(data: list, recalibrate: bool = False):
     """
     Retrieve all numbers from the data as a list of lists of digits
     :param data: The data as a list of lines
+    :param recalibrate: Whether to use the recalibrated data method
     :return: The numbers as a list of lists of digits
     """
     ret_list = []
-    all_numbers = [[num for num in re.findall(r"\d+", line)] for line in data]
+
+    if recalibrate:
+        expanded_strings = list(STRING_MAP.keys()) + list(STRING_MAP.values())
+        all_numbers = [[num for num in re.findall(r"(?=("+'|'.join(expanded_strings)+r"))", line)] for line in data]
+    else:
+        all_numbers = [[num for num in re.findall(r"\d+", line)] for line in data]
     
     for numbers in all_numbers:
         separated_numbers = []
         for number in numbers:
-            separated_numbers += [digit for digit in number]
+            if recalibrate:
+                if number in STRING_MAP.keys():
+                    separated_numbers.append(STRING_MAP[number])
+                else:
+                    separated_numbers += [digit for digit in number]
+            else:
+                separated_numbers += [digit for digit in number]
         ret_list.append(separated_numbers)
 
     return ret_list
@@ -56,17 +77,18 @@ def merge_digit_pairs(digits: list):
     return digit_pairs
 
 
-def sum_pairs():
+def sum_pairs(recalibrate: bool = False):
     """
     Find all numbers in the data, merge the first and last digits of each number 
         into a list of pairs, and return the sum of all pairs
+    :param recalibrate: Whether to use the recalibrated data method
     :return: The sum of all pairs
     """
     data = parse_data(
         data_string=read_data(pathname="input.txt")
     )
 
-    digits = find_numbers(data=data)
+    digits = find_numbers(data=data, recalibrate=recalibrate)
     pairs = merge_digit_pairs(digits=digits)
 
     return sum(pairs)
