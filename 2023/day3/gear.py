36e905de9e1ded76d70aa49d912d545d4f74a6b9
shai-github <shaigithub@gmail.com>
2023-12-05 21:27:28 -0600

    digit spans work

--- 2023/day3/gear.py
+++ 2023/day3/gear.py
@@ -1,5 +1,7 @@
 import re
 
+from collections import defaultdict
+
 
 def read_data(pathname: str) -> str:
     """
@@ -20,19 +22,47 @@ def parse_data(data_string: str) -> list:
     return data_string.split("\n")
 
 
-def get_number_spans(data: list) -> dict:
+def get_number_spans(line: list) -> dict:
     """
     Gets the index spans of all numbers in a line
-    :param data: The data as a list of lines
+    :param line: line of input data
     :return: a dictionary mapping a line index to the positional spans 
         of numbers in a given line
     """
+    ref_int = 0
+    ret_dict = defaultdict()
+
+    ref_dict = {
+        idx: num for idx, num in enumerate(line) if num.isdigit()
+    }
+
+    for idx, key in enumerate(ref_dict.keys()):
+        if idx == 0:
+            ret_dict[ref_int] = {
+                'number': ref_dict[key],
+                'pos': [key]
+            }
+            last_int = key
+        else:
+            if key == last_int + 1:
+                ret_dict[ref_int]['number'] += ref_dict[key]
+                ret_dict[ref_int]['pos'].append(key)
+                last_int = key
+            else:
+                ref_int += 1
+                ret_dict[ref_int] = {
+                    'number': ref_dict[key],
+                    'pos': [key]
+                }
+                last_int = key
+
+    return ret_dict
 
 
-def get_symbol_indices(data: list) -> dict:
+def get_symbol_indices(line: list) -> dict:
     """
     Gets the index of all symbols in a line
-    :param data: The data as a list of lines
+    :param line: line of input data
     :return: a dictionary mapping a line index to the positions of symbols
         in a given line
     """
