dcb96038296c4b69ce806af97eeb45bf2071afa8
shai-github <shaigithub@gmail.com>
2023-12-26 18:19:01 -0500

    mapping by index

--- 2023/day5/fertilizer.py
+++ 2023/day5/fertilizer.py
@@ -34,7 +34,8 @@ def map_data(data: list) -> dict:
 
     for idx, line in enumerate(data):
         if idx == 0:
-            data_map["seeds"] = line.replace("seeds: ", "").split(" ")
+            string_num = line.replace("seeds: ", "").split(" ")
+            data_map["seeds"] = [int(num) for num in string_num]
         elif idx in data_breaks:
             db_next = data_breaks.index(idx) + 1
             if db_next == len(data_breaks):
@@ -42,18 +43,56 @@ def map_data(data: list) -> dict:
             else:
                 ref_data = data[idx + 1: data_breaks[db_next]]
 
-            data_map[data[idx].replace(":", "")] = [line.split(" ") for line in ref_data if line != ""]
+            string_num = [line.split(" ") for line in ref_data if line != ""]
+            data_map[data[idx].replace(":", "")] = [[int(num) for num in line] for line in string_num]
     
     return data_map
     
+
+def pair_mapping(num_list: list) -> dict:
+    """
+    Maps destination range starts to source range start according to
+        a given range length. 
+    :param num_list: The list of numbers to map
+    :return: The mapping of destination range starts to source range starts
+    """
+    num_map = defaultdict()
+
+    for mapping in num_list:
+        destination = list(range(mapping[0], mapping[0] + mapping[2]))
+        source = list(range(mapping[1], mapping[1] + mapping[2]))
+        num_map.update(dict(zip(source, destination)))
+
+    return num_map
+
+
+def make_mappings(data_map: dict) -> dict:
+    """
+    Calculate the mappings for each of the data types. Order matters
+        for the final mappings of seeds to location, so the order of
+        the input data is mapped according to index.
+    :param data_map: The input data as a dictionary
+    :return: The mappings for each of the data types
+    """
+    return {
+        idx: pair_mapping(num_list=val) for idx, val in enumerate(data_map.values())
+    }
+
+
+def traverse_mappings(mappings: dict) -> dict:
+    """
+    """
+
 
 def something():
     """
     """
-    data = parse_data(read_data("input.txt"))
-    data_map = map_data(data=data)
+    data_map = map_data(data=parse_data(read_data("test.txt")))
+    mappings = make_mappings(
+        data_map={k: v for k, v in data_map.items() if k != "seeds"}
+    )
 
-    return data_map
+    return data_map, mappings
 
 
 if __name__ == "__main__":
