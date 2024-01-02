6df31bf720a9ef7220bf0c49eb8ee99f38e09b44
shai-github <shaigithub@gmail.com>
2024-01-02 08:05:24 -0600

    upload test

--- 2023/day5/fertilizer.py
+++ 2023/day5/fertilizer.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-
+import time
 
 def read_data(pathname: str) -> str:
     """
@@ -20,10 +20,11 @@ def parse_data(data_string: str) -> list:
     return data_string.split("\n")
 
 
-def map_data(data: list) -> dict:
+def map_data(data: list, use_range: bool = False) -> dict:
     """
     Maps the data into a dictionary format
     :param data: The data as a list of lines
+    :param use_range: Whether to use the range of values for seeds
     :return: The data split out into items for seeds, seed-to-soil map, 
         soil-to-fertilizer map, fertilizer-to-water map, water-to-light map,
         light-to-temperature map, temperature-to-humidity map, and
@@ -35,7 +36,11 @@ def map_data(data: list) -> dict:
     for idx, line in enumerate(data):
         if idx == 0:
             string_num = line.replace("seeds: ", "").split(" ")
-            data_map["seeds"] = [int(num) for num in string_num]
+            int_list = [int(num) for num in string_num]
+            if use_range:
+                data_map["seeds"] = list(zip(int_list[0::2], int_list[1::2]))
+            else:
+                data_map["seeds"] = int_list
         elif idx in data_breaks:
             db_next = data_breaks.index(idx) + 1
             if db_next == len(data_breaks):
@@ -80,13 +85,14 @@ def make_mappings(data_map: dict) -> dict:
     }
 
 
-def traverse_mappings(seeds: list, mappings: dict) -> dict:
+def traverse_by_seed(seeds: list, mappings: dict) -> dict:
     """
-    Traverses the mappings for seeds to soil, soil to fertilizer,
+    Traverses the mappings for individuals seeds to soil, soil to fertilizer,
         fertilizer to water, water to light, light to temperature,
         temperature to humidity, and humidity to location.
     :param seeds: The seeds to map
     :param mappings: The mappings for each of the data types
+    :param use_range: Whether to use the range of values for seeds
     :return: The mapping of seeds to location
     """
     location_map = defaultdict()
@@ -108,26 +114,120 @@ def traverse_mappings(seeds: list, mappings: dict) -> dict:
     return location_map
 
 
-def find_minimum():
+def traverse_by_range(seeds: list, mappings: dict) -> dict:
+    """
+    Traverses the mappings for seed ranges to soil, soil to fertilizer,
+        fertilizer to water, water to light, light to temperature,
+        temperature to humidity, and humidity to location.
+    :param seeds: The seeds to map
+    :param mappings: The mappings for each of the data types
+    :param use_range: Whether to use the range of values for seeds
+    :return: a list of seeds
+    """
+    reduced_mappings = []
+
+    for mapping in mappings.values():
+        for destination, source in mapping.items():
+            reduced_mappings.append([destination, source["source"][0], source["range"]])
+
+    
+
+
+    # print(map_list)
+    locations = []
+
+    for seed_range in seeds:
+        seed_range.append((seed_range[0] + seed_range[1] - 1))
+        range_ref = [seed_range]
+        next_pass = []
+
+        while range_ref:
+            ref = range_ref.pop()
+            for _, mapping in mappings.items():
+                for destination, source in mapping.items():
+                    if (ref[0] > source["source"][1]) or (ref[2] < source["source"][0]):
+                        continue
+                    elif source["source"][1] > ref[2] >= ref[0] >= source["source"][0]:
+                        delta = ref[0] - source["source"][0]
+
+
+
+
+
+                    if seed_range[0] > source["source"][1]:
+                        continue
+                    elif (seed_range[0] >= source["source"][0]) and (seed_range[0] <= source["source"][1]):
+                        if (seed_range[0] + seed_range[1] - 1) <= source["source"][1]:
+                            next_pass.append(((seed_range[0] + destination - source["source"][0]), seed_range[1]))
+                        elif (seed_range[0] + seed_range[1] - 1) > source["source"][1]:
+                            range_delta = (seed_range[0] + seed_range[1] - 1) - source["source"][1]
+                            next_pass.append(
+                                (seed_range[0] + destination, source["source"][1] - seed_range[0] + 1), 
+                            )
+                            range_ref.append((source["source"][1] + 1, range_delta))
+                        break
+                    elif seed_range[0] < source["source"][0]:
+                        if (seed_range[0] + seed_range[1] - 1) < source["source"][0]:
+                            continue
+                        else:
+                            range_delta = source["source"][0] - seed_range[0]
+                            if (seed_range[0] + seed_range[1] - 1) <= source["source"][1]:
+                                next_pass.append( 
+                                    ((seed_range[0] + range_delta + destination), ((seed_range[0] + seed_range[1] - 1) - source["source"][0]))
+                                )
+                                range_ref.append((seed_range[0], range_delta))
+                            elif (seed_range[0] + seed_range[1] - 1) > source["source"][1]:
+                                post_range = (seed_range[0] + seed_range[1] - 1) - source["source"][1]
+                                next_pass.append(
+                                    ((seed_range[0] + range_delta + destination), ((seed_range[0] + seed_range[1] - 1) - source["source"][0]))
+                                )
+                                range_ref.append((seed_range[0], range_delta))
+                                range_ref.append((source["source"][1], post_range))
+                        break
+                else:
+                    range_ref.append(ref)
+        range_ref = next_pass
+        next_pass = []
+
+    locations.extend(range_ref)
+    print(locations)
+
+    range_set = set(range_ref)
+    return min([r[0] for r in range_set])
+
+
+def find_minimum(use_range: bool = False):
     """
     Retrieves the minimum location value from mapping the seeds to
         locations via the mappings for seeds to soil, soil to fertilizer,
         fertilizer to water, water to light, light to temperature,
         temperature to humidity, and humidity to location.
+    :param use_range: Whether to use the range of values for seeds
     :return: The minimum location value
     """
-    data_map = map_data(data=parse_data(read_data("input.txt")))
+    data_map = map_data(
+        data=parse_data(read_data("test.txt")),
+        use_range=use_range
+    )
+
     mappings = make_mappings(
         data_map={k: v for k, v in data_map.items() if k != "seeds"}
     )
 
-    location_map = traverse_mappings(
+    if use_range:
+        return traverse_by_range(
+            seeds=data_map["seeds"], 
+            mappings=mappings,
+        )
+
+    location_map = traverse_by_seed(
         seeds=data_map["seeds"], 
-        mappings=mappings
+        mappings=mappings,
     )
 
     return min(location_map.values())
 
 
 if __name__ == "__main__":
-    print("The minimum location value is:", find_minimum())
+    print("The minimum location value for seeds only is:", find_minimum())
+    print("The minimum location value for seed ranges is:", find_minimum(use_range=True))
